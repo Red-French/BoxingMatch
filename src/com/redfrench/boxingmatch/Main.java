ef526f28c105202c0759e327ce31ad8d04dcfd8c
Red-French <red@redfrench.net>
2017-05-31 00:16:23 -0500

    move player setup to its own class

--- src/com/redfrench/boxingmatch/Main.java
+++ src/com/redfrench/boxingmatch/Main.java
@@ -1,41 +1,24 @@
 package com.redfrench.boxingmatch;
 
 import java.util.Scanner;
+import com.redfrench.boxingmatch.Player_setup;
 
  
-public class Main {
+public class Main extends Player_setup {
 
   public static void main(String[] args) {
 
     System.out.println("\n LET'S GET READY TO RUMMMMBUUUUULLE!");
 
-    int i;
+    Player_setup setup = new Player_setup();
+    setup.questions();
+    
+    
+    
 
-    // question the player
-    for(i = 0; i < 2; i++) {
-
-      @SuppressWarnings("resource")
-      Scanner scanner = new Scanner(System.in);
-
-      System.out.print("\nEnter your name: ");
-      String name = scanner.nextLine();                  
-
-      System.out.println("Enter your best punch: ");
-      String punch = scanner.nextLine();
-         
-      System.out.print("Enter your weight: ");
-      int weight = scanner.nextInt();                          
-
-      // create new fighter
-      Fighter competitor = new Fighter(name, weight, punch);
-
-      announcer(competitor, i);
-
-    }
-
-      if(i == 2) {
-        System.out.println("\nDING DING DING DING DING!!!!!");
-      }
+//      if(i == 2) {
+//        System.out.println("\nDING DING DING DING DING!!!!!");
+//      }
 
   //  String[] fighters = {name, punch};
 
@@ -44,17 +27,5 @@ public class Main {
   //    .forEach(System.out::println);
   }
 
-
-  public static void announcer(Fighter competitor, int i) {
-   
-    if(i == 0) {
-      System.out.println("\nIn this corner, ");
-    } else {
-      System.out.println("\nIn the opposite corner, ");
-    }
-    
-    System.out.println("weighing in at " + competitor.getWeight() + " with his best punch being the " + competitor.getBestPunch() + "... " + competitor.getName() + "!!!");
-
-  }
        
 }
