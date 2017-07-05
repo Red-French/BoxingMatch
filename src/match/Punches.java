068cdfe9354b805e22ae05529bdd50fbce0ad5c8
TFIC\fb001940 <red.french@fbitn.com>
2017-07-05 14:03:37 -0500

    remove Timer and endOfRound() class (functionality moved to Round class)

--- src/match/Punches.java
+++ src/match/Punches.java
@@ -1,16 +1,7 @@
 package match;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
-import referee.Round;
-
-
 public class Punches {
 	
-	Round round = new Round();
-	
-	Timer timer;
 	int body = -1;
 	int jab = -2;
 	int hook = -3;
@@ -19,19 +10,10 @@ public class Punches {
 	int bestPunch = -7;  // TODO: determine if a fighter threw his best punch
 	
 	public void throwPunches() throws InterruptedException {
-		timer = new Timer();
-		timer.schedule(new EndOfRound(), 5000);
-		for(int i = 0; i < 50000; i++) {
+		for(int i = 0; i < 5; i++) {
 			Thread.sleep(1000);
 			System.out.println(i + " punch, punch, jab, jab");
 		}
 	}
-	
-	class EndOfRound extends TimerTask {
-        public void run() {
-        timer.cancel();
-        round.endOfRound();
-        }    
-    }
 	
 }
