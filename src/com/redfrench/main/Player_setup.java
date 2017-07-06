c17a789f6ed73f3014c5115fe947811a1f7c882b
Red-French <red@redfrench.net>
2017-07-05 19:05:51 -0500

    make duksters private and add setter; remove static from duksters getter

--- src/com/redfrench/main/Player_setup.java
+++ src/com/redfrench/main/Player_setup.java
@@ -6,18 +6,21 @@ import java.util.Scanner;
 
 public class Player_setup {
 
-	int i;
-	public static ArrayList<Fighter> getDuksters() {
+	public ArrayList<Fighter> getDuksters() {
 		return duksters;
 	}
 
+	private ArrayList<Fighter> setDuksters(Fighter competitor) {
+		duksters.add(competitor);
+		return null;
+	}
 
-	static ArrayList<Fighter> duksters = new ArrayList<Fighter>();
+	private static ArrayList<Fighter> duksters = new ArrayList<Fighter>();
 
 	public void questions() {
 		
 	    // question the player
-	    for(i = 0; i < 2; i++) {
+	    for(int i = 0; i < 2; i++) {
 	
 	      @SuppressWarnings("resource")
 	      Scanner scanner = new Scanner(System.in);
@@ -34,35 +37,24 @@ public class Player_setup {
 	      // create new fighter
 	      Fighter competitor = new Fighter(name, weight, punch);
 	      
-	      duksters.add(competitor);
-	
-	      if(i == 1) {
-	    	  announcer(competitor);
-	      }
+	      setDuksters(competitor);
 
 	    }
 	}
     
     
-    public static void announcer(Fighter competitor) {
-//    	String stngHolder = "weighing in at " + competitor.getWeight() + " with his best punch being the " + competitor.getBestPunch() + "... " + competitor.getName() + "!!!";
-//        if(i == 0) {
-//          System.out.print("\nIn this corner, " + stngHolder);
-//        } else {
-//          System.out.print("\nIn the opposite corner, " + stngHolder);
-          for(Fighter duksters : duksters) {
+    public void announcer() {  // TODO: create Announcer class for this method
+    						   // TODO: write method for end of fight announcement
+    	for(Fighter duksters : duksters) {
         	  System.out.print("\nWeighing in at " + duksters.getWeight()  + " with his best punch being the " + duksters.getBestPunch() + "... " + duksters.getName() + "!!!\n\n");
           }
           
           System.out.println("\n~ TONIGHT'S FIGHT TICKET ~\n");
-          
           for(Fighter duksters : duksters) {
 	    	  System.out.println("FIGHTER: " + duksters.getName());
 	    	  System.out.println("WEIGHT: " + duksters.getWeight());
 	    	  System.out.println("BEST PUNCH: " + duksters.getBestPunch() + "\n\n");
 	     }
         }
-        
-//        System.out.println("weighing in at " + competitor.getWeight() + " with his best punch being the " + competitor.getBestPunch() + "... " + competitor.getName() + "!!!");  
-    
+
 }
