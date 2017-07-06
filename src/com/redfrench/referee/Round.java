417d5be3471c8cfbddb142a8a07bc83ed05f1759
Red-French <red@redfrench.net>
2017-07-05 19:08:57 -0500

    change to use composition to access Player_setup (removed static in Player_setup class

--- src/com/redfrench/referee/Round.java
+++ src/com/redfrench/referee/Round.java
@@ -2,26 +2,48 @@ package com.redfrench.referee;
 
 import com.redfrench.main.Player_setup;
 import com.redfrench.match.Punches;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Round {
-
+public class Round extends Punches {
+    
+    Player_setup player = new Player_setup();
+    
+    Timer timer;
     int round = 0;
  
     public void newRound() throws InterruptedException {
-    	Punches punches = new Punches();
-    	
     	System.out.println("Come out fighting!");
-    	
-    	punches.throwPunches();
+    	timer = new Timer();
+    	timer.schedule(new BeginRound(), 2000);
     }
     
-    public void endOfRound() {
+    public void endRound() throws InterruptedException {
     	this.round = round +1;
     	System.out.println("\nDING DING DING DING DING!!!!! \nEnd of round " + round + "!");
     	System.out.println("\nEND OF ROUND " + round + " BOXER REPORT\n");
-    	System.out.print(Player_setup.getDuksters().get(0).getName() + "'s ENERGY: ");
-    	System.out.println(Player_setup.getDuksters().get(0).getEnergy());
-    	System.out.print(Player_setup.getDuksters().get(1).getName() + "'s ENERGY: ");
-    	System.out.println(Player_setup.getDuksters().get(1).getEnergy() + "\n");
+    	System.out.print(player.getDuksters().get(0).getName() + "'s ENERGY: ");
+    	System.out.println(player.getDuksters().get(0).getEnergy());
+    	System.out.print(player.getDuksters().get(1).getName() + "'s ENERGY: ");
+    	System.out.println(player.getDuksters().get(1).getEnergy() + "\n");
+    	if (this.round < 4) {
+    		// TODO: add countdown to next round beginning
+    		timer.schedule(new BeginRound(), 3000);
+    	} else {
+    		timer.cancel();
+    	}
     }
+    
+    public class BeginRound extends TimerTask {
+        public void run() {
+        try {
+			throwPunches();
+			endRound();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        }    
+    }
+
 }
