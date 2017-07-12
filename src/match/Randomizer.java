0c33764e6f2c94d03a153e9afad48dd9c582e800
Red-French <red@redfrench.net>
2017-07-11 22:32:38 -0500

    add randomizer for time between punches

--- src/match/Randomizer.java
+++ src/match/Randomizer.java
@@ -1,13 +1,19 @@
 package match;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Randomizer {
+	Random randomGenerator = new Random();
 	
 	public int randomNum(int num) {
-		Random randomGenerator = new Random();
     		int randomInt;
 			return randomInt = randomGenerator.nextInt(num);
+	}
+	
+	public int randomNumGen() {
+		int timeBetweenPunches;
+		return timeBetweenPunches = ThreadLocalRandom.current().nextInt(250, 4000 + 1);
 	}
 
 }
