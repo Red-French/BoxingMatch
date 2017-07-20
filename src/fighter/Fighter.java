d1aa6971a6b78a04d5d17d719c87d85c36d3242d
Red-French <red@redfrench.net>
2017-07-19 21:12:16 -0500

    add 'score' property w/ getters/setters

--- src/fighter/Fighter.java
+++ src/fighter/Fighter.java
@@ -1,4 +1,5 @@
-package main;
+package fighter;
+
 
 public class Fighter {
 
@@ -6,20 +7,25 @@ public class Fighter {
 	private int weight;
 	private String bestPunch;
 	private float energy;
+	private float score;  // score of each round (reset to 10 after each round)
+	private int numberOfRoundsWon;  // total # of rounds won
 	
 	public Fighter(String name, int weight, String bestPunch) {
-		this(100, name, weight, bestPunch);  // send default energy with parameters
+		this(100, 10, 0, name, weight, bestPunch);  // send default energy, score, and numberOfRoundsWon with parameters
 	}
 	
-	public Fighter(float energy2, String name, int weight, String bestPunch) {
+	public Fighter(float energy, float score, int roundsWon, String name, int weight, String bestPunch) {
 		super();
 		this.name = name;
 		this.weight = weight;
 		this.bestPunch = bestPunch;
-		this.energy = energy2;
+		this.energy = energy;
+		this.score = score;
+		this.numberOfRoundsWon = roundsWon;
 	}
 	
 	
+	// GETTERS
 	public String getName() {
 		return name;
 	}
@@ -32,10 +38,29 @@ public class Fighter {
 	public float getEnergy() {
 		return energy;
 	}
-
+	public float getScore() {
+		return score;
+	}
+	public int getNumberOfRoundsWon() {
+		return numberOfRoundsWon;
+	}
+	
+	
+	// SETTERS
 	public float setEnergy(float punchPower) {
 		energy = energy - punchPower;
 		return energy;
 	}
-
+	public float setScore(float roundsScore) {
+		score += roundsScore;
+		return score;
+	}
+	public float resetScore() {
+		return score = 10;
+	}
+	public int setNumberOfRoundsWon(int roundWon) {
+		numberOfRoundsWon += roundWon;
+		return numberOfRoundsWon;
+	}
+	
 }
