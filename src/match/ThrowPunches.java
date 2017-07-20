e1cbed9788b4034e0ee0d4d31ba838ce08f839d2
Red-French <red@redfrench.net>
2017-07-19 21:23:37 -0500

    import Values, PunchTypes, Score, Commentator, KnockoutStatus

--- src/match/ThrowPunches.java
+++ src/match/ThrowPunches.java
@@ -1,70 +1,96 @@
 package match;
 
 import main.Player_setup;
-import referee.Announcer;
+import main.Values;
+import punches.PunchTypes;
+import punches.Score;
+import referee.Commentator;
+import match.KnockoutStatus;
+
 
 public class ThrowPunches {
 	
-	Randomizer randomizer = new Randomizer();
-	PunchTypes punchTypes = new PunchTypes();
-	Announcer announcer = new Announcer();
-	TrashTalk trashTalk = new TrashTalk();
 	private static Player_setup player = new Player_setup();
+	private Randomizer randomizer = new Randomizer();
+	private PunchTypes punchTypes = new PunchTypes();
+	private Commentator commentator = new Commentator();
+	private TrashTalk trashTalk = new TrashTalk();
+	private Score score = new Score();
+	private KnockoutStatus knockoutStatus = new KnockoutStatus();
 	
 	
 	public void throwPunches() throws InterruptedException {
 
+		int boxerPunching;
+		int boxerPunched;
+		int punch;
+		int strengthOfPunch;
+		int boxerBanter;
+		boolean boxerTrashTalk;
+		String activeBoxersName;
+		String boxersBestPunch;
+		String thisPunch;
+		float punchPower;
+		
 		long time = System.currentTimeMillis();
-		long end = time + 10000;  // 10-second rounds
+		long end = time + Values.lengthOfRounds;
+		
 		
 		while(System.currentTimeMillis() < end) {
 			
-			int boxer = randomizer.randomNum(2);
-			int punch = randomizer.randomNum(6);
-			int strengthOfPunch = randomizer.randomNum(4);
-			int boxerBanter = randomizer.randomNum(80);
-			boolean boxerTrashTalk = (boxerBanter % 20 == 0) ? true : false;
+			boxerPunching = randomizer.randomNum(2);
+			punch = randomizer.randomNum(6);
+			strengthOfPunch = randomizer.randomNum(4);
+			boxerBanter = randomizer.randomNum(80);
+			boxerTrashTalk = (boxerBanter % 20 == 0) ? true : false;
 	
-			String activeBoxer = player.getDuksters().get(boxer).getName();
-			String boxersBestPunch = (String) player.getDuksters().get(boxer).getBestPunch();
-			String thisPunch = punchTypes.getThepunch().get(punch).getPunchType();
-			float punchPower = punchTypes.getThepunch().get(punch).getPunchPower();
+			activeBoxersName = player.getDuksters().get(boxerPunching).getName();
+			boxersBestPunch = (String) player.getDuksters().get(boxerPunching).getBestPunch();
+			thisPunch = punchTypes.getThepunch().get(punch).getPunchType();
+			punchPower = punchTypes.getThepunch().get(punch).getPunchPower();
+
 			
-			if (strengthOfPunch != 3) { // remove energy from boxer who got punched
-				if (boxer == 0) {
-					if (!thisPunch.equals(boxersBestPunch)) {
-						player.getDuksters().get(1).setEnergy(punchPower);
-					} else {  // boxer threw his best punch
-						int powerOfBestPunch = (int) punchTypes.getThepunch().get(6).getPunchPower();
-						player.getDuksters().get(1).setEnergy(powerOfBestPunch);
-					}
-				} 
-				else { // remove energy from boxer who got punched
-					if (!thisPunch.equals(boxersBestPunch)) {
-						player.getDuksters().get(0).setEnergy(punchPower);
-					} else {  // boxer threw his best punch
-						int powerOfBestPunch = (int) punchTypes.getThepunch().get(6).getPunchPower();
-						player.getDuksters().get(0).setEnergy(powerOfBestPunch);
-					}
+			if (boxerPunching == 0) {
+				boxerPunched = 1;
+			} else {
+				boxerPunched = 0;
+			}
+			
+			
+			// set vars in Commentator class
+			commentator.setBoxerPunching(boxerPunching);
+			commentator.setBoxerPunched(boxerPunched);
+			
+			score.scoreRound(thisPunch, boxerPunched);
+			
+			
+			// if boxer landed punch, remove energy from boxer who got punched
+			if (strengthOfPunch != 3) {
+				if (!thisPunch.equals(boxersBestPunch)) {
+					player.getDuksters().get(boxerPunched).setEnergy(punchPower);
+				} else {  // boxer threw his best punch
+					int powerOfBestPunch = (int) punchTypes.getThepunch().get(6).getPunchPower();
+					player.getDuksters().get(boxerPunched).setEnergy(powerOfBestPunch);
 				}
 			} else {  // remove energy from boxer who threw a missed punch
-				if (boxer == 0) {
-					float powerOfMissedPunch = (float) (punchPower/2);
-					player.getDuksters().get(0).setEnergy(powerOfMissedPunch);
-				} 
-				else {
-					float powerOfMissedPunch = (float) (punchPower/2);
-					player.getDuksters().get(1).setEnergy(powerOfMissedPunch);
-				}
+				float powerOfMissedPunch = (float) (punchPower/2);
+				player.getDuksters().get(boxerPunching).setEnergy(powerOfMissedPunch);
 			}
 			
 			// vary time between punches
 			int timeBetweenPunches = randomizer.randomNumGen();
 			Thread.sleep(timeBetweenPunches);
 			
-			announcer.commentator(activeBoxer, boxersBestPunch, thisPunch, strengthOfPunch);
+			commentator.duringRound(activeBoxersName, boxersBestPunch, thisPunch, strengthOfPunch);
+			
+			// if a boxer's energy is below knockoutLevel, end fight
+			if(( player.getDuksters().get(0).getEnergy() < Values.knockoutLevel) || (player.getDuksters().get(1).getEnergy() < Values.knockoutLevel)) {
+				knockoutStatus.setKnockout(true);
+				break;
+			}
+			
 			if (boxerTrashTalk) {
-				trashTalk.banter(boxer);
+				trashTalk.banter(boxerPunching);
 			}
 		}
 	}
