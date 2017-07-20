267af01d12fbf263f6934871ae21fb5e98a60697
Red-French <red@redfrench.net>
2017-07-19 21:17:29 -0500

    add comments

--- src/match/Crowd.java
+++ src/match/Crowd.java
@@ -1,9 +1,13 @@
-package crowd;
+package match;
 
-public class Crowd {
+public class Crowd {  // TODO: Crowd
+	
+	// * new thread?
+	
 	// cheers
 	// boos
 	// throws things
 	// causes riot after decision
+	// going to sleep
 
 }
