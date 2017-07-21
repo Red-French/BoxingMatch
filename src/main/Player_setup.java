0ab35d1fc8527706854bc60843c9d1adbdcd2254
Red-French <red@redfrench.net>
2017-07-20 21:13:49 -0500

    verify user input for # of rounds is a num

--- src/main/Player_setup.java
+++ src/main/Player_setup.java
@@ -10,35 +10,30 @@ public class Player_setup {
 
 	private static ArrayList<Fighter> duksters = new ArrayList<Fighter>();
 	private String name;
-	private boolean allLetters;
-	private int punchVal;
 	private int weight;
 	private String punch;
-	
 
 	public void questions() {
 		
 	    // question the player
 	    for(int i = 0; i < 2; i++) {
-	      punchVal = 0;
-	      allLetters = false;
+	      int punchVal = 0;
+	      boolean allLetters = false;
 	      
 	      @SuppressWarnings("resource")
 	      Scanner scanner = new Scanner(System.in);
 	      
+	      // GET BOXER'S NAME
+	      System.out.print("\nEnter your name: ");
 	      while(!allLetters) {
-	    	  System.out.print("\nEnter your name: ");
 	    	  name = scanner.nextLine();
-	    	  for (char c : name.toCharArray()) {
-	    		  if(!Character.isLetter(c)) {
-			    	System.out.println("Please enter letters only.");
-			    	continue;
-	    		  } else {
-	    			  allLetters = true;
-	    		  }
+	    	  allLetters = name.matches("[a-zA-Z]+");
+	    	  if(!allLetters) {
+	    		  System.out.println("Letters only.");
 	    	  }
 	      }
 	      
+	      // GET BOXER'S BEST PUNCH
 	      while(punchVal < 1 || punchVal > 6) {
 	    	  System.out.println("Enter number of your best punch (1-6): \n"
 	      		+ "1 - body blow \n"
@@ -47,12 +42,14 @@ public class Player_setup {
 	      		+ "4 - combination \n"
 	      		+ "5 - low blow \n"
 	      		+ "6 - uppercut");
+	    	  
+		      while(!scanner.hasNextInt()) {
+		    	  System.out.println("Numbers 1-6 only.");
+		          scanner.next();  // non-integer; consume it and continue
+		      }
 	    	  punchVal = scanner.nextInt();
 	      }
-	      
-	      System.out.print("Enter your weight: ");
-	      weight = scanner.nextInt();
-	      
+	    	  
 	      switch(punchVal) {
 	      	case 1:
 	          punch = "body blow";
@@ -73,20 +70,37 @@ public class Player_setup {
 	          punch = "uppercut";
 	          break;
 	      };
+
 	  
-	 
+	      // GET BOXER'S WEIGHT
+	      System.out.print("Enter your weight: ");
+	      while(!scanner.hasNextInt()) {
+	    	  System.out.println("Numbers only.");
+	          scanner.next();  // non-integer; consume it and continue
+	      }
+	      weight = scanner.nextInt();
+
+	      
 	      // create new fighter
 	      Fighter competitor = new Fighter(name, weight, punch);
 	      setDuksters(competitor);
 
 	    }
 	    
+	    // GET # OF ROUNDS
 	    @SuppressWarnings("resource")
 	    Scanner scanner = new Scanner(System.in);
+	    System.out.print("\nHow many rounds? (1-10): ");
+	    while(!scanner.hasNextInt()) {
+	    	  System.out.println("Numbers only.");
+	          scanner.next();  // non-integer; consume it and continue
+	      }
 	    while(Values.numRoundsToFight < 1 || Values.numRoundsToFight > 10) {
 	    	System.out.print("\nHow many rounds? (1-10): ");
 	    	Values.numRoundsToFight = scanner.nextInt();
 	    }
+	    
+	    
 	    Values.calcKnockoutLevel();
 	    System.out.println("The knockout level is " + Values.knockoutLevel);
 	}
