a27503569a94bfaecaf109b545d1c6f6a4fed30a
Red-French <red@redfrench.net>
2017-07-22 19:42:56 -0500

    rename method for clarity

--- src/match/Randomizer.java
+++ src/match/Randomizer.java
@@ -5,15 +5,15 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Randomizer {
 	Random randomGenerator = new Random();
-	
+
 	public int randomNum(int num) {
-    		int randomInt;
-			return randomInt = randomGenerator.nextInt(num);
+		int randomInt;
+		return randomInt = randomGenerator.nextInt(num);
 	}
 	
-	public int randomNumGen() {
+	public int randomTimeBetweenPunches() {
 		int timeBetweenPunches;
-		return timeBetweenPunches = ThreadLocalRandom.current().nextInt(250, 4000 + 1);
+		return timeBetweenPunches = ThreadLocalRandom.current().nextInt(250, 4000);  // returns random int between these values  
 	}
 
 }
